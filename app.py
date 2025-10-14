b4ae9c0b257f34ae9546fabeaa558ec0bc2ad8cf
Ryuichi Takano <45957617+tknrych@users.noreply.github.com>
2025-10-14 15:29:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import urllib.parse
+import json
 
 # 各ページの描画関数をインポート
 from views.search_interface import display_search_interface
@@ -39,6 +40,7 @@ text_to_analyze_encoded = query_params.get("analyze_text")
 term_to_search_encoded = query_params.get("search_term")
 text_to_check_encoded = query_params.get("check_text")
 original_text_encoded = query_params.get("original_text") 
+reference_treaties_encoded = query_params.get("reference_treaties")
 
 # パラメータに応じて描画するページを切り替え
 if treaty_id_to_display:
@@ -52,7 +54,10 @@ elif term_to_search_encoded:
 elif text_to_check_encoded:
     decoded_text = urllib.parse.unquote(text_to_check_encoded)
     original_text = urllib.parse.unquote(original_text_encoded)
-    display_check_page(decoded_text, original_text)
+    decoded_references = []
+    if reference_treaties_encoded:
+        decoded_references = json.loads(urllib.parse.unquote(reference_treaties_encoded))
+    display_check_page(decoded_text, original_text, decoded_references)
 else:
     # デフォルトはメインの検索インターフェース
     display_search_interface()
